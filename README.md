a22a1d288c3a2b9bbfdc1427440fbd32a4181b43
Micah Culpepper <micahculpepper@gmail.com>
2019-06-10 10:30:13 -0500

    First working version

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # swarmcreds
+
+Show the secrets stored in your Docker Swarm.
+
+## Example
+
+```
+[root@docker-host ~]# ./swarmcreds.sh
+apikey	9FuBmQbw
+passwd	AVJN7Ckq
+```
+
+## Motivation
+
+Sometimes passwords, SSL keys, etc. change and will need to be udpated in a production environment.
+Updating a swarm secret requires restarting the affected services twice. I wanted a way to ensure
+parity between my swarm secrets and my other password stores without forcing service restarts.
+There are docker commands to set/update swarm secrets, but no commands to show existing secrets,
+so I made one.
+
+## Requirements
+
+- Linux
+- Docker
+- A POSIX compliant shell at `/bin/sh`
+
+
+## Installation
+
+1. Download [swarmcreds.sh](https://github.com/micahculpepper/swarmcreds/raw/master/swarmcreds.sh) and save it
+wherever you like.
+
+2. Make sure `swarmcreds.sh` is executable: `chmod +x swarmcreds.sh`
+
+3. Symlink `swarmcreds.sh` to a program directory. Example:
+
+```bash
+# From the directory containing swarmcreds.sh:
+ln -s "$(pwd)/swarmcreds.sh" /usr/local/bin/swarmcreds
+```
+
+## Usage
+
+Simply execute the script as a user with Docker permissions. Output is tab-separated. 
+There are no option flags.
+
+## Method
+
+This script works by using `docker secret ls` to get a list of all existing secret names, 
+spinning up a new docker service that has access to all of the secrets, and then reading them.
---
+++ swarmcreds.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+
+# MIT License
+#
+# Copyright (c) 2019 Micah Culpepper
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+set -e
+
+
+# Get a list of all credential names
+
+names=$(docker secret ls --format '{{.Name}}')
+if [ -z "$names" ]; then
+    (>&2 echo "No swarm secrets to check.")
+    exit 0
+fi
+
+
+# Build the image we will use for accessing credentials
+
+cat << EOF | docker build -t swarmcreds:latest . -f -
+FROM alpine:latest
+CMD ["sh", "-c", "while /bin/true ; do sleep 5; done"]
+EOF
+
+
+# Create a docker-compose file in memory and use it to deploy a new stack on this host
+# that will give us access to the credentials we're looking for
+
+nodeid=$(docker node ls --format '{{.Self}}\t{{.ID}}' | awk '/true/ {print $2}')
+NL=$(printf "\n")
+secretlist=""
+secretsection=""
+for name in $names; do
+    secretlist="${secretlist}      - ${name}${NL}"
+    secretsection="${secretsection}  ${name}:${NL}    external: true${NL}"
+done
+
+cat << EOF | docker stack deploy -c - swarmcreds
+version: '3.7'
+services:
+  swarmcreds:
+    image: swarmcreds
+    deploy:
+      placement:
+        constraints:
+          - node.id == ${nodeid}
+    secrets:
+${secretlist}
+secrets:
+${secretsection}
+EOF
+
+
+# Get the ID of the running container we just made
+
+containerid=$(\
+    docker inspect $(docker stack ps -q swarmcreds) \
+    --format '{{.Status.ContainerStatus.ContainerID}}')
+
+
+# Use the new container to gather credentials
+
+for name in $names; do
+    secret=$(docker exec "$containerid" /bin/cat "/run/secrets/${name}")
+    printf "${name}\t${secret}\n"
+done
+
+
+# Clean up
+
+docker stack rm swarmcreds
